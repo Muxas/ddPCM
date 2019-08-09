abba79786c45494e5271110fab424cb7c8bd24d2
Aleksandr Mikhalev <aleksandr.mikhalev@kaust.edu.sa>
2019-08-09 14:15:13 +0200

    Now mydx.py extracts nbasis x nbasis matrix for each pair of spheres

--- mydx.f90
+++ mydx.f90
@@ -55,7 +55,7 @@ contains
         call dx(nbasis*nsph, x, y)
     end subroutine ddpcm_dx
 
-    subroutine gen_mat(nbasis, ngrid, nsph, mat)
+    subroutine gen_mat_ngrid(nbasis, ngrid, nsph, mat)
         implicit none
         integer, intent(in) :: nbasis, ngrid, nsph
         real*8, intent(out) :: mat(ngrid, nsph, nbasis, nsph)
@@ -104,6 +104,70 @@ contains
                         end do
                         tt = tt / tij
                     end do
+                end do
+            end do
+        end do
+        deallocate(vts, vplm, basloc, vcos, vsin, stat=istatus)
+        if(istatus .ne. 0) then
+            write(6,*) 'gen_mat: deallocation failed !'
+            stop
+        end if
+    end subroutine gen_mat_ngrid
+
+    subroutine gen_mat(nbasis, nsph, mat)
+        implicit none
+        integer, intent(in) :: nbasis, nsph
+        real*8, intent(out) :: mat(nbasis, nsph, nbasis, nsph)
+        real*8, allocatable :: vts(:,:), vplm(:), basloc(:), vcos(:), vsin(:)
+        real*8 :: c(3), vij(3), sij(3)
+        real*8 :: vvij, tij, fourpi, tt, f, f1
+        integer :: its, isph, jsph, l, m, ind, lm, istatus
+
+        allocate(vts(ngrid, nbasis), vplm(nbasis), basloc(nbasis), &
+            & vcos(lmax+1), vsin(lmax+1), stat=istatus)
+        if(istatus .ne. 0) then
+            write(6,*) 'gen_mat: allocation failed !'
+            stop
+        end if
+        fourpi = four * pi
+
+        do isph = 1, nsph
+            do jsph = 1, nsph
+                if(isph .eq. jsph) then
+                    mat(:, isph, :, jsph) = zero
+                    cycle
+                end if
+                do its = 1, ngrid
+                    if(ui(its, isph) .eq. zero) then
+                        vts(its, :) = zero
+                        !mat(its, isph, :, jsph) = zero
+                        cycle
+                    end if
+                    ! build the geometrical variables
+                    c = csph(:, isph) + rsph(isph)*grid(:, its)
+                    vij = c - csph(:, jsph)
+                    vvij = sqrt(dot_product(vij, vij))
+                    tij = vvij / rsph(jsph)
+                    sij = vij / vvij 
+                    ! build the local basis
+                    call ylmbas(sij, basloc, vplm, vcos, vsin)
+                    ! with all the required stuff, finally compute
+                    ! the "potential" at the point
+                    tt = one / tij
+                    do l = 0, lmax
+                        ind = l*l + l + 1
+                        f = fourpi * dble(l) / (two*dble(l) + one) * tt * &
+                            & ui(its, isph)
+                        do m = -l, l
+                            vts(its, ind+m) = f * basloc(ind+m)
+                            !mat(its, isph, ind+m, jsph) = f * basloc(ind+m)
+                            ! write(*, *) its, isph, ind+m, jsph, mat(its, isph, ind+m, jsph)
+                        end do
+                        tt = tt / tij
+                    end do
+                end do
+                do ind = 1, nbasis
+                    call intrhs(isph, vts(:, ind), mat(:, isph, ind, jsph))
                 end do
             end do
         end do
