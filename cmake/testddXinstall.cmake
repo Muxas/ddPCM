56b4d7a87a36f9c4a53459bb261a16a7221b77ad
Nigel91 <42963496+Nige91@users.noreply.github.com>
2021-03-19 12:34:59 +0100

    n

--- cmake/testddXinstall.cmake
+++ cmake/testddXinstall.cmake
@@ -5,4 +5,9 @@ project(ddx_test LANGUAGES Fortran)
 find_package(ddX REQUIRED)
 add_executable(dd_core "dd_core.f90")
 
+set(ddx_incl ${ddX_DIR}/../../include/ddX)
+message(${ddx_incl})
+
+include_directories(ddx_test ${ddx_incl})
+
 target_link_libraries(dd_core ddx)
