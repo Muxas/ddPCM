71dd3905b07a36406a5582a45914c52fe588da45
Nige91 <42963496+Nige91@users.noreply.github.com>
2021-03-26 15:24:18 +0100

    Update testddXinstall.cmake

--- cmake/testddXinstall.cmake
+++ cmake/testddXinstall.cmake
@@ -3,7 +3,7 @@ cmake_minimum_required(VERSION 3.2.3)
 project(ddx_test LANGUAGES Fortran)
 
 find_package(ddX REQUIRED)
-add_executable(dd_core "dd_core.f90")
+add_executable(dd_core "ddx_core.f90")
 
 get_target_property(ddx_incl ddx INTERFACE_INCLUDE_DIRECTORIES)
 message(${ddx_incl})
